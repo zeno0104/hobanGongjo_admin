eaa7ac49c1a1828e74bfe91d4e274aa314608b55
zenoo97 <jaehun4086@naver.com>
2025-02-14 17:09:21 +0900

    [Update] manifest 수정 > id 추가

--- hobanGongjo_admin/src/firebase/notifications.ts
+++ hobanGongjo_admin/src/firebase/notifications.ts
@@ -1,13 +1,6 @@
-import { getToken } from "firebase/messaging";
-import { messaging } from "./firebase";
-
 export async function requestNotificationPermission() {
-  const permission = await Notification.requestPermission();
-  if (permission === "granted") {
-    const token = await getToken(messaging, {
-      vapidKey:
-        "BPFSOm498XIcQEO53qcTksmAFTT-LxSdLm3nrnXr0N1Wpwv-OG4INTtg6KdqJp3XzbZTnewzDj2vj6ULM6bkTxE",
-    });
-    console.log("FCM Token:", token);
+  if ("Notification" in window) {
+    const permission = await Notification.requestPermission();
+    console.log("Notification permission status:", permission);
   }
 }
