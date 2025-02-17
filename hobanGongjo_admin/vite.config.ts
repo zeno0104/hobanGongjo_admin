09c3c9a9ae33bca13b626785ecb85faf64e4ac34
zenoo97 <jaehun4086@naver.com>
2025-02-17 16:01:48 +0900

    [Fix] 새로고침시 404 에러 수정

--- hobanGongjo_admin/vite.config.ts
+++ hobanGongjo_admin/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // 원하는 포트 설정
+  },
+  build: {
+    outDir: "dist", // 빌드 디렉토리 설정
+  },
+  base: "/", // 기본 URL 설정 (PWA에서 필요)
 });
