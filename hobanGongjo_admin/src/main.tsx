7bcf88e3e479a4b965506ab942e1869547ce1744
zenoo97 <jaehun4086@naver.com>
2025-02-17 16:05:35 +0900

    [Update] local 수정

--- hobanGongjo_admin/src/main.tsx
+++ hobanGongjo_admin/src/main.tsx
@@ -3,19 +3,6 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "../core/notification/settingFCM";
-
-// 서비스 워커 등록 (PWA 새로고침 문제 해결)
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered successfully:", registration);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
-
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
